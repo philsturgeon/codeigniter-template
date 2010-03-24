5bd7a79bd445dcf349c136c6ca97dac7f0e8b1a2
philsturgeon <email@philsturgeon.co.uk>
2010-03-24 11:40:30 +0000

    Now supports a config file that allows a default theme to be set and "Theme locations" to be used. That means you could have application/themes as one and third_party/themes as another.

---
+++ config/template.php
@@ -0,0 +1,34 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Theme
+|--------------------------------------------------------------------------
+|
+| Which theme to use by default?
+| This can be overriden with $this->template->set_theme('foo');
+|
+|	some_theme
+|
+|   Default: ''
+|
+*/
+
+$config['theme'] = 'default';
+
+/*
+|--------------------------------------------------------------------------
+| Theme
+|--------------------------------------------------------------------------
+|
+| Where should we expect to see themes?
+|
+|	Default: array(APPPATH.'themes/' => '../themes/')
+|
+*/
+
+$config['theme_locations'] = array(
+	APPPATH.'themes/' => '../themes/',
+);
+
+?>
